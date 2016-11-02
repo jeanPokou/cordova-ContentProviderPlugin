e50a04304f84bee9b2b5b0e6a503abe147bca532
jeanpokou <pokoujeanmarc@gmail.com>
2016-11-02 12:04:17 -0300

    plugin for ELD, forked from phearme/cordova-ContentProviderPlugin

---
+++ www/ContentProviderPlugin.js
@@ -0,0 +1,23 @@
+function ContentProviderPlugin() {
+}
+
+var pluginMethods = [
+	"query", "updateRegister"
+];
+
+pluginMethods.forEach(function (pluginMethod) {
+	ContentProviderPlugin.prototype[pluginMethod] = function (jsonArg, successCallback, errorCallback) {
+		cordova.exec(successCallback, errorCallback, "ContentProviderPlugin", pluginMethod, [jsonArg]);
+	};
+});
+
+ContentProviderPlugin.install = function () {
+  if (!window.plugins) {
+    window.plugins = {};
+  }
+
+  window.plugins.contentproviderplugin = new ContentProviderPlugin();
+  return window.plugins.contentproviderplugin;
+};
+
+cordova.addConstructor(ContentProviderPlugin.install);
